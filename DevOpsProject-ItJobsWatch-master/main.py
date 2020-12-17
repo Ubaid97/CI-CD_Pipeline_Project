c9befc4708890c02d2860ada2397b34de3bf1bd6
Ubaid-ur-rehman Muhammad <zdac091@live.rhul.ac.uk>
2020-12-17 15:51:40 +0000

    changes to imports in main

--- DevOpsProject-ItJobsWatch-master/main.py
+++ DevOpsProject-ItJobsWatch-master/main.py
@@ -1,4 +1,8 @@
-from src.cmd_user_interface import CmdUserInterface
+# from src.cmd_user_interface import CmdUserInterface
+
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '.', 'src'))
+import cmd_user_interface
 
 if __name__ == '__main__':
-    CmdUserInterface()
+    cmd_user_interface.CmdUserInterface()
