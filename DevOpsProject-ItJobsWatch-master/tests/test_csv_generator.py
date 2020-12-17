04bcb1ff53f07a2b521b8618a9e30d2526bcb46d
Ubaid-ur-rehman Muhammad <zdac091@live.rhul.ac.uk>
2020-12-17 15:35:03 +0000

    fixed bugs

--- DevOpsProject-ItJobsWatch-master/tests/test_csv_generator.py
+++ DevOpsProject-ItJobsWatch-master/tests/test_csv_generator.py
@@ -56,7 +56,7 @@ class TestCSVGenerator:
             for line in parsed_csv_file:
                 list_count += 1
 
-        assert list_count/2 == 30
+        assert list_count == 30
 
     def test_csv_with_headers(self):
         list_count = 0
@@ -65,7 +65,7 @@ class TestCSVGenerator:
             for line in parsed_csv_file:
                 list_count += 1
 
-        assert list_count/2 == 31
+        assert list_count == 31
 
     def test_csv_without_headers_default_location(self):
         list_count = 0
@@ -74,4 +74,4 @@ class TestCSVGenerator:
             for line in parsed_csv_file:
                 list_count += 1
 
-        assert list_count/2 == 30
+        assert list_count == 30
